50c984eccd04a5c872d39428887405b710b7102d
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-10-05 14:05:15 +0200

    feat(routing): setting up middleware to redirect from secure pages

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+	const token = req.cookies.get("authToken");
+
+	if (
+		!token &&
+		req.nextUrl.pathname !== "/login" &&
+		req.nextUrl.pathname !== "/register"
+	) {
+		return NextResponse.redirect(new URL("/login", req.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/((?!login|register).*)"], // Matches all routes except /login and /register
+};
