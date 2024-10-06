c8d9fc5367c79968b1abf50c04c270b4ec3263db
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-10-06 12:41:07 +0200

    feat(usr): creating authentication header

---
+++ src/app/(auth)/__components/TheHeaderAuthentication.tsx
@@ -0,0 +1,5 @@
+export const TheHeaderAuthentication = () => (
+	<header className="w-full py-4 md:py-12">
+		<h1 className="text-white-primary">Finn</h1>
+	</header>
+);
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react";
 
+import { TheHeaderAuthentication } from "./__components/TheHeaderAuthentication";
+
 export default function AuthLayout({ children }: { children: ReactNode }) {
 	return (
-		<main className="h-screen w-screen overlfow-hidden flex items-center px-3 md:px-20 bg-black-primary">
-			{children}
-		</main>
+		// @ts-ignore Weird UMD global error because TS14 not supporting Fragments shorthand <> right now.
+		<>
+			<TheHeaderAuthentication />
+			<main className="h-screen w-screen overlfow-hidden flex items-center px-3 md:px-20 bg-black-primary">
+				{children}
+			</main>
+		</>
 	);
 }
